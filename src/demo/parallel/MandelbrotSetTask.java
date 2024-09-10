b3a1892ee8269cc0d099f40d1aba459f4ad7ecb3
WadimAndrianov <slaveofGod23@outlook.com>
2024-09-10 18:18:59 +0300

    modifield gamma

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -42,7 +42,7 @@ import javafx.scene.paint.Color;
  * Task to render Mandelbrot set using given parameters. See {@link 
  * #MandelbrotRendererTask(boolean, javafx.scene.image.PixelWriter, int, int, 
  * double, double, double, double, double, double, double, double, boolean) 
- * constructor} for parameters list. The task returns time in milliseconds as 
+ * constructor} for parameters list. The task returns time in milliseconds as
  * its calculated value.
  * 
  * <p><i>
@@ -351,11 +351,11 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
+            Color.rgb(0, 40, 0),
             Color.RED,
             Color.WHITE,
             Color.RED,
-            Color.rgb(100, 0, 0),
+            Color.rgb(0, 0, 100),
             Color.RED,
             Color.rgb(50, 0, 0)
         };
